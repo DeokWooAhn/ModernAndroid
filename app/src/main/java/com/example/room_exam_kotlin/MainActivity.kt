0fa5252be355e3f3c72b97da47bb8e92cd1ebfef
DoekWooAhn <fjrlvkdlxj@naver.com>
2021-10-28 16:36:31 +0900

    coroutine으로 room 비동기, ViewModel 추가

--- app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
@@ -3,29 +3,30 @@ package com.example.room_exam_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase :: class.java, "database-name"
-        )
-            .allowMainThreadQueries()
-            .build()
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         // 관찰하면서 UI 변경
-        db.todoDao().getAll().observe(this, Observer {
+        viewModel.getAll().observe(this, Observer {
             tv.text = it.toString()
         })
 
-
         btn.setOnClickListener {
-            db.todoDao().insert(Todo(edt.text.toString()))
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.insert(Todo(edt.text.toString()))
+            }
         }
     }
 }
