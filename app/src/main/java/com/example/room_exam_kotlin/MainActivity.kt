92e4b1ac0c1934d3f41f3de69cb176bc38521929
DoekWooAhn <fjrlvkdlxj@naver.com>
2021-10-28 18:35:09 +0900

    dataBinding

--- app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/room_exam_kotlin/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.room_exam_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
+import com.example.room_exam_kotlin.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,19 +17,11 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.lifecycleOwner = this
 
-        // 관찰하면서 UI 변경
-        viewModel.getAll().observe(this, Observer {
-            tv.text = it.toString()
-        })
-
-        btn.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                viewModel.insert(Todo(edt.text.toString()))
-            }
-        }
+        val viewModel: MainViewModel by viewModels()
+        binding.viewModel = viewModel
     }
 }
